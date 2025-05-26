4a290fe251a4ff0ce899254b77f362d3f11e8a68
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2025-05-26 13:55:35 +1200

    add footer component

---
+++ src/lib/footer.svelte
@@ -0,0 +1,12 @@
+<footer>
+  <p class="foot">&copy; Emma Fitzgerald Craighead Diocesan School 2025</p>
+</footer>
+
+<style>
+  .foot {
+    padding: 15px;
+    font-size: 20px;
+    background-color: #495d63;
+    color: #eaf9ff;
+  }
+</style>
