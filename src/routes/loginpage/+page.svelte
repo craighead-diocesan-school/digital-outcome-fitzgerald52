ba402d0d179f61c5b6f932e3e75273b61065f225
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2025-05-26 14:05:57 +1200

    adds fire base db and auth
    db.js connects to firebase and servised provides functions to rest of app.
    works in chrome users able to add to database and login

---
+++ src/routes/loginpage/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Header from '$lib/Header.svelte'
+  import Footer from '../../lib/footer.svelte'
+  import { addplan, getplans, login } from '$lib/db.js'
+  import { user } from '$lib/state.svelte.js'
+</script>
+
+<main class="columns">
+  <div class="column is-one-third">
+    <p>Find information on all 12 of New Zealand Universities and directly compare different options and plan for your future without stress and confusion</p>
+  </div>
+  <div class="column">
+    <Header />
+    <button
+      class="button is-primary is-large"
+      on:click={() => {
+        login(login)
+      }}
+      >login
+    </button>
+  </div>
+</main>
+<nav>
+  <a href="/searchpage">searchpage</a>
+  <a href="/loginpage">loginpage</a>
+</nav>
+<Footer />
+
+<style>
+  p {
+    padding: 35px;
+    padding-top: 80px;
+    padding-bottom: 80px;
+    font-size: 40px;
+    background-color: #eaf9ff;
+  }
+  .is-large {
+    width: 300px;
+    height: 125px;
+    font-size: 75px;
+    margin-top: 75px;
+  }
+</style>
---
+++ src/routes/searchpage/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Header from '$lib/Header.svelte'
+  import Footer from '../../lib/footer.svelte'
+  import { addplan, getplans, login } from '$lib/db.js'
+  let content = [
+    {
+      uni: '',
+      cost: '',
+      prerequisits: '',
+      name: '',
+    },
+    {
+      uni: '',
+      cost: '',
+      prerequisits: '',
+      name: '',
+    },
+    {
+      uni: '',
+      cost: '',
+      prerequisits: '',
+      name: '',
+    },
+  ]
+</script>
+
+<Header />
+
+<nav>
+  <a href="/searchpage">searchpage</a>
+  <a href="/loginpage">loginpage</a>
+</nav>
+<input type="text" placeholder="Search.." name="search" />
+<button type="submit"><img src="loupe.png" alt="" /></button>
+
+<table>
+  <thead>
+    <tr>
+      <th>course</th>
+      <th>University</th>
+      <th>cost</th>
+      <th>prerequisits</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      {#each content as course}
+        <th>{course.name}</th>
+        <th>{course.uni}</th>
+        <th>{course.cost}</th>
+        <th>{course.prerequisits}</th>
+      {/each}
+    </tr>
+  </tbody>
+</table>
+
+<Footer />
+
+<style>
+  img {
+    width: 15px;
+  }
+</style>
