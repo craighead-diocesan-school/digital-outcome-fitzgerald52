542c24de4f057624acaeea96d50576009ab18ab9
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2025-05-26 13:55:52 +1200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,61 @@
 <script>
   import Header from '$lib/Header.svelte'
   import BulmaExamples from '$lib/BulmaExamples.svelte'
+  import { addplan, getplans, login } from '$lib/db.js'
+
+  let plans = getplans()
+
+  let newplan = {
+    course: '',
+    cost: '',
+    location: '',
+  }
 </script>
 
+<nav>
+  <a href="/loginpage">loginpage</a>
+</nav>
 <Header />
 
 <main class="content section">
-  <h2>SvelteKit</h2>
+  {#await plans}
+    <p>Loading...</p>
+  {:then plans}
+    {#each plans as plan}
+      <p>{plan.course} {plan.cost} {plan.location}</p>{/each}
+  {/await}
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  <label class="label">
+    course: <input bind:value={newplan.course} />
+  </label>
 
-  <p>This template comes loaded with the <a href="https://bulma.io/documentation/">Bulma CSS framework</a>, so you can save time and focus on your project.</p>
+  <label class="label">
+    cost: <input type="number" bind:value={newplan.cost} />
+  </label>
 
-  <p>Here's some examples of colour helpers. You can change the colours in <code>theme.scss</code></p>
-  <BulmaExamples />
+  <label class="label">
+    location: <input bind:value={newplan.location} />
+  </label>
+
+  <button
+    class="button"
+    on:click={() => {
+      addplan(newplan)
+    }}
+  >
+    Save
+  </button>
 </main>
-
 <footer class="footer">
   <p class="has-text-centered">&copy; Craighead Diocesan School 2025</p>
 </footer>
+
+<!-- use java script variables eg 
+content {
+  title 
+  subtitle
+  uni
+  cost
+  prerequisits 
+}
+then use <header> <title>title.content</title></header> -->
